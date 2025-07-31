00488f523d59a57ae409382dd3e8b2872cf92f0b
Hasmoonn <mohamedhasmoon175@gmail.com>
2025-07-31 22:58:04 +0530

    some components and logics are added, hero, latest sections are added

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -9,7 +9,9 @@ const Navbar = () => {
 
   return (
     <div className='flex items-center justify-between py-5 font-medium'>
-      <img src={assets.logo}className="w-36" alt="" />
+      <Link to='/'>
+        <img src={assets.logo} className='w-28 sm:w-36' alt="" />
+      </Link>
 
       <ul className='hidden sm:flex gap-5 text-sm text-gray-700'>
         <NavLink to="/" className="flex flex-col items-center gap-1">
@@ -34,10 +36,10 @@ const Navbar = () => {
       </ul>
 
       <div className='flex items-center gap-6'>
-        <img src={assets.search_icon} className='w-5 cursor-pointer' alt="" />
+        <img src={assets.search_icon} className='w-4 sm:w-5 cursor-pointer' alt="" />
 
         <div className='group relative'>
-          <img src={assets.profile_icon} className='w-5 cursor-pointer' alt="" />
+          <img src={assets.profile_icon} className='w-4 sm:w-5 cursor-pointer' alt="" />
 
           <div className='absolute hidden group-hover:block right-0 dropdown-menu pt-4'>
             <div className='flex flex-col gap-2 w-36 py-3 px-5 bg-slate-100 text-gray-500 rounded-md'>
@@ -49,7 +51,7 @@ const Navbar = () => {
         </div>
 
         <Link to='/cart' className='relative' >
-          <img src={assets.cart_icon} className='w-5 min-w-5' alt="" />
+          <img src={assets.cart_icon} className='w-4 sm:w-5 min-w-4' alt="" />
           <p className='absolute right-[-5px] top-[-5px] w-4 text-center bg-black text-white leading-4 rounded-full aspect-square text-[8px]'>10</p>
         </Link>
 
