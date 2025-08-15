fb47864e60ef655abfa789a4d07b5c8d7d323bf8
Hasmoonn <mohamedhasmoon175@gmail.com>
2025-08-16 04:48:25 +0530

    admin folder created and normal frontend modified

---
+++ admin/src/pages/Add.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Add = ({token}) => {
+  return (
+    <div>Add</div>
+  )
+}
+
+export default Add
---
+++ admin/src/pages/List.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const List = ({token}) => {
+  return (
+    <div>List</div>
+  )
+}
+
+export default List
